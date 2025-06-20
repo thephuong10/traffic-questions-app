c2fcc9a32a018fca8476e0e0912f7210ce34b780
phuotran1 <lhuynh@strongtie.com>
2025-06-20 10:38:39 +0700

    change type of test

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -8,7 +8,7 @@ function MainPage({ tests }) {
         <div className="cards-container">
           {tests.map((test, idx) => (
             <div className="card">
-              <Link to={`/test/${idx}`} key={idx} style={{ textDecoration: 'none', width:'100%' }}>
+              <Link to={`/test-hard/${idx}`} key={idx} style={{ textDecoration: 'none', width:'100%' }}>
                 <div className="card-content">
                   <h2>Đề {idx + 1}</h2>
                   <p>25 câu hỏi</p>
